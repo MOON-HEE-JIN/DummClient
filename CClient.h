700089d8744fcfbe87dd9b8efb81bee79141dbcd
MOON-HEE-JIN <GIT.CODE.MHJ@gmail.com>
2026-02-23 17:30:10 +0900

    Develop (#9)
    * merge feature/organize
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * test comment
    
    * Feature/changepid to develop (#3)
    
    * merge feature/develop
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * add changepid
    
    * Refactoring/dummy client structure (#6)
    
    * Develop (#4)
    
    * merge feature/organize
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * test comment
    
    * Feature/changepid to develop (#3)
    
    * merge feature/develop
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * add changepid
    
    * 더미, 클라이언트 및 로그 리팩토링
    
    * Feature/dummy loopback (#8)
    
    * Develop (#7)
    
    * merge feature/organize
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * test comment
    
    * Feature/changepid to develop (#3)
    
    * merge feature/develop
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * add changepid
    
    * Refactoring/dummy client structure (#6)
    
    * Develop (#4)
    
    * merge feature/organize
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * test comment
    
    * Feature/changepid to develop (#3)
    
    * merge feature/develop
    
    * Class Dummy and Organize Code
    
    * Add DisConnect/ReConnect
    
    * add changepid
    
    * 더미, 클라이언트 및 로그 리팩토링
    
    * Zone 변경시 패킷 보내기 않도록 수정

--- CClient.h
+++ CClient.h
@@ -17,35 +17,38 @@ public:
 	{
 		CloseSocket();
 	}
-	void ReConnect(const char IP[16], unsigned short Port, HANDLE cicp)
-	{
-		Connect(IP, Port, cicp);
-	}
+	void ReConnect(const char IP[16], unsigned short Port, HANDLE cicp);
 
 private:
 	int m_nClientID;		// 클라이언트 구분 ID
-	int m_nTestZoneID;		// 클라이언트 Zone 구분 ID
+	int m_nZoneID;		// 클라이언트 Zone ID
+	int m_nPreZoneID;
 	int m_nInZoneClientID;	// Dummy 에서 관리하는 Zone 내에서의 자기 vector index
 
-	bool m_bWaitServerResponse;		// 서버 응답을 기다리는 상태
+	std::atomic<bool> m_bWaitServerResponse;		// 서버 응답을 기다리는 상태
 
 	__int64 m_LoopbackData;
 	int m_nSendReqDelayTime;
 	int m_nLastSendReqTime;
 
-	int m_nDisConnectRandomCount;
 	int m_nLoopbackSendCount;
+	int m_nDisConnectRandomCount;
+	
+	int m_nChangeZoneIDCount;
+	int m_nChangeZoneIDRequestCount;
 
 	int m_nServerProcID;
 public:
 	int GetSendReqDelayTime() const { return m_nSendReqDelayTime; }
 	__int64 GetLoopbackData() const { return m_LoopbackData; }
-	int GetServerProcID() { return m_nTestZoneID; }
+	int GetZoneID() { return m_nZoneID; }
+	int	GetClientID() { return m_nClientID; }
 
 	void CompletedWaitServerResponse() { m_bWaitServerResponse = false; }
 
 	__int64 IncrementLoopbackData() { return ++m_LoopbackData; }
 	bool IncrementDisConnectRandomCount();
-
+	void ChangeZoneIDRequest();
+	void ReStoreZoneID() { m_nZoneID = m_nPreZoneID; };
 	bool IsSend();
 };
